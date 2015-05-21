8dd5295103fcee8026facab6a41422c5a31bfcc7
IOOII <yourproductdirect@gmail.com>
2015-05-21 14:58:40 -0400

    Added reviews to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,10 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    @reviews = @place.reviews
+    @review = Review.new
+
+
   end
 
   # GET /places/new
